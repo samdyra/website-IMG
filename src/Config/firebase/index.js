1aef3f99c3d69615700c2db72b653ffb895c3390
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2022-07-27 22:42:07 +0700

    fixing firebase

---
+++ src/Config/firebase/index.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABh8qcPATUszEeSj-Zvg1n45LvKyJaOOA",
+  authDomain: "img-itb.firebaseapp.com",
+  projectId: "img-itb",
+  storageBucket: "img-itb.appspot.com",
+  messagingSenderId: "42127320999",
+  appId: "1:42127320999:web:6609ae0a09345b21d6b882",
+  measurementId: "G-V5D8TKJSLV",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
