c070638f420db7c7dca2287936f62927c060411b
raihanmakarim <raihanmakarim21@gmail.com>
2022-08-31 02:16:37 +0700

    add homepage index+scss

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import style from "./style.module.scss";
+import earth from '../../assets/images/earth.png';
+
 
 const HomeScreen = () => {
-  const { container } = style;
-  return <div></div>;
+  const { container, desc, logo, welcome } = style;
+  return (
+    <div className={container}>
+      <div className={welcome}>
+        <h2>Welcome to IMG-ITB Official Website</h2>
+      </div>
+      <div className={logo}>
+        <div className={desc}>
+        <h1>IKATAN MAHASISWA</h1> 
+        <h1>GEODESI-ITB</h1>
+        <p>blablablablablablabalbalbalbalbablal</p>
+        </div>
+        <img src={earth}/>
+      </div>
+    </div>
+  );
 };
 
 export default HomeScreen;
