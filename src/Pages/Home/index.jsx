1d511ce754fc63eb0ded6e4a6684323642dd6176
raihanmakarim <raihanmakarim21@gmail.com>
2022-08-31 18:28:41 +0700

    add section 2

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,9 +4,19 @@ import earth from '../../assets/images/earth.png';
 
 
 const HomeScreen = () => {
-  const { container, desc, logo, welcome } = style;
+  const { 
+    container,
+    desc, 
+    logo, 
+    welcome, 
+    containerSection,
+    judulSection2,
+    descSection2,
+    nilai 
+  } = style;
   return (
     <div className={container}>
+      <div className={containerSection}>
       <div className={welcome}>
         <h2>Welcome to IMG-ITB Official Website</h2>
       </div>
@@ -16,6 +26,29 @@ const HomeScreen = () => {
         <p>blablablablablablabalbalbalbalbablal</p>
         </div>
         <img src={earth}/>
+      </div>
+      </div>
+      <div className={containerSection}>
+        <div className={judulSection2}>
+          <h1>Asas, Sifat, dan Tujuan IMG-ITB</h1>
+        </div>
+        <div className={descSection2} >
+          <div className={nilai} >
+            <img src={earth}/>
+            <h2>Asas</h2>
+            <p>blablablabla</p>
+          </div>
+          <div className={nilai} >
+            <img src={earth}/>
+            <h2>Sifat</h2>
+            <p>blablablabla</p>
+          </div>
+          <div className={nilai} >
+            <img src={earth}/>
+            <h2>Tujuan</h2>
+            <p>blablablabla</p>
+          </div>
+        </div>
       </div>
     </div>
   );
