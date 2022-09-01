15f78b5596295a26401ff2265e9f0ce4a7a1aff3
raihanmakarim <raihanmakarim21@gmail.com>
2022-09-01 16:09:18 +0700

    changes to activities page

--- src/Pages/Activities/index.jsx
+++ src/Pages/Activities/index.jsx
@@ -2,11 +2,52 @@ import React from "react";
 import style from "./style.module.scss";
 
 const Activities = () => {
-    const { container} = style;
+    const { 
+    containerSection,
+    box1,
+    box2
+    } = style;
     return(
-        <div className={container}>
+        <div>
+        <div className={containerSection}>
+        <div >
             <h1>Internal Events in IMG-ITB</h1>
         </div>
+        <div>
+            <div className={box1}>
+                asdasdasd
+            </div>
+            <div className={box2}>
+
+            </div>
+        </div>
+        <div>
+            <h1>Our Internal Event Dates</h1>
+            <button>click here</button>
+        </div>
+        </div>
+
+        <div className={containerSection}>
+
+
+        </div>
+
+        <div className={containerSection}>
+
+
+        </div>
+
+        <div className={containerSection}>
+
+
+        </div>
+
+
+
+
+
+        </div>
+
     );
 };
 
