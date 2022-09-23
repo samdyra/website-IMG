926d6331acf8ee6849c517c0a2290c14b36051a5
raihanmakarim <raihanmakarim21@gmail.com>
2022-09-23 15:47:05 +0700

    add footer style

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,39 +3,58 @@ import style from "./style.module.scss";
 import LogoIMG from "../../assets/images/LogoIMG.png";
 
 const Footer = () => {
-  const { container, content, logo, logotext, bottom, karya } = style;
+  const {
+    container,
+    content,
+    logo,
+    logotext,
+    bottom,
+    karya,
+    contenttop,
+    container2,
+  } = style;
   return (
     <div className={container}>
-      <div className={content}>
-        <div>
-          <div className={logo}>
-            <img src={LogoIMG} />
-            <div className={logotext}>
-              <h1>Ikatan Mahasiswa Geodesi</h1>
-              <h1>Institut Teknologi Bandung</h1>
+      <div className={container2}>
+        <div className={contenttop}>
+          <div>
+            <div className={logo}>
+              <img src={LogoIMG} />
+              <div className={logotext}>
+                <h1>Ikatan Mahasiswa Geodesi</h1>
+                <h1>Institut Teknologi Bandung</h1>
+              </div>
             </div>
+          </div>
+
+          <div className={logotext}>
+            <h1>#ERATBERDAMPAK</h1>
           </div>
         </div>
 
-        <div className={logotext}>
-          <h1>#ERATBERDAMPAK</h1>
+        <div className={content}>
+          <div>
+            <h2>Connect With Us</h2>
+          </div>
+          <div>
+            <h2>Aktivitas</h2>
+            <h3>Internal</h3>
+            <h3>Wisuda IMG</h3>
+            <h3>Geopoint</h3>
+            <h3>Student Chapter</h3>
+          </div>
+          <div className={karya}>
+            <h2>Karya</h2>
+            <h3>Georeference</h3>
+            <h3>Locus</h3>
+            <h3>Geosphere</h3>
+            <h3>IMG-X</h3>
+          </div>
         </div>
-      </div>
 
-      <div className={content}>
-        <div>
-          <h3>Connect With Us</h3>
+        <div className={bottom}>
+          <h1>Copyright © 2022 IMG-ITB | All rights reserved.</h1>
         </div>
-        <div>
-          <h3>Aktivitas</h3>
-        </div>
-        <div className={karya}>
-          <h3>Karya</h3>
-        </div>
-      </div>
-
-      <div className={bottom}>
-        <h1>Copyright © 2022 IMG-ITB | All rights reserved.</h1>
       </div>
     </div>
   );
