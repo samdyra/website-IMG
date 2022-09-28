b547e4b14f2aff0a4684dc489ea8931ac4dbb6db
raihanmakarim <raihanmakarim21@gmail.com>
2022-09-29 05:47:20 +0700

    add activity pages

--- src/Pages/KabarKesenatoran/index.jsx
+++ src/Pages/KabarKesenatoran/index.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import style from "./style.module.scss";
+import Header from "../../Components/HeaderPage";
+import Gallery from "../../Components/Slider";
 
 const kabarKesenatoran = () => {
   const { container, containerSection, box1, box2 } = style;
 
   return (
     <div>
+      <Header />
       <div className={containerSection}>
-        <h1>Kabar Kesenatoran</h1>
+        <Gallery sliderName="verticalcard" />
       </div>
+      <div className={containerSection}>Media Kesenatoran</div>
     </div>
   );
 };
