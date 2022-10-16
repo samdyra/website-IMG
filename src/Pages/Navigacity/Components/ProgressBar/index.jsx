d5e317087c2609df75e087a66ba468591a1b4cec
raihanmakarim <raihanmakarim21@gmail.com>
2022-10-16 10:58:19 +0700

    navigacity progressbar style change

--- src/Pages/Navigacity/Components/ProgressBar/index.jsx
+++ src/Pages/Navigacity/Components/ProgressBar/index.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import style from "./style.module.scss";
 
 const ProgressBar = () => {
-  const { progressBarContainer, progressBar, bar, dots, dotsContainer } = style;
+  const {
+    progressBarContainer,
+    progressBar,
+    bar,
+    dots,
+    dotsContainer,
+    dotsActive,
+  } = style;
   return (
     <div className={progressBarContainer}>
       <div className={bar}></div>
       <div className={progressBar}>
         <div className={dotsContainer}>
           <h2 style={{ width: "150px" }}>15 Oktober 2022</h2>
-          <div className={dots}></div>
+          <div className={dotsActives}></div>
           <h2 style={{ width: "150px" }}>Pembukaan penjualan tiket</h2>
         </div>
         <div className={dotsContainer}>
@@ -27,14 +34,14 @@ const ProgressBar = () => {
         <div className={dotsContainer}>
           <h2 style={{ width: "150px" }}>1 Februari 2023</h2>
           <div className={dots}></div>
-          <h2 style={{ width: "250px", height: "40px" }}>
+          <h2 style={{ width: "250px", height: "60px" }}>
             Pembagian race pack
           </h2>
         </div>
         <div className={dotsContainer}>
           <h2 style={{ width: "150px" }}>4 Februari 2023</h2>
           <div className={dots}></div>
-          <h2 style={{ width: "250px" }}>
+          <h2 style={{ width: "22s0px" }}>
             Hari pelaksanaan dan pengumuman pemenang
           </h2>
         </div>
