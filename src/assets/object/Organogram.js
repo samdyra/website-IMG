b75c3b06af56f40eb1892aa72f5a48246f0279b8
alshaadiq <alshaaadiq@gmail.com>
2024-01-03 00:25:02 +0700

    Compress ketum.jpg, import organogram from url

--- src/assets/object/Organogram.js
+++ src/assets/object/Organogram.js
@@ -1,9 +1,8 @@
-import organogrambph from "../images/organogrambph.png";
-//import organogramSenator from "../images/senatorOrg.png";
-import organogramGeopoint from "../images/geopointOrg.png";
+//import organogrambph from "../images/organogrambph.png";import organogramSenator from "../images/senatorOrg.png";import organogramGeopoint from "../images/geopointOrg.png";
+
 const SliderData = [
-  { image: organogrambph, },
-  { image: organogramGeopoint, }
+  { image: "https://gdurl.com/1G1y",  },   //Organogram BPH
+  { image: "https://gdurl.com/WzNQ",  }    //Organogram Geopoint
 ]
 
 export default SliderData
