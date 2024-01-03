ef63537acd3225b2e7c41c199947bb50f08290a6
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2024-01-03 11:06:14 +0700

    fix: build fixes

--- src/assets/object/Organogram.js
+++ src/assets/object/Organogram.js
@@ -1,8 +1,6 @@
-//import organogrambph from "../images/organogrambph.png";import organogramSenator from "../images/senatorOrg.png";import organogramGeopoint from "../images/geopointOrg.png";
-
 const SliderData = [
-  { image: "https://gdurl.com/1G1y",  },   //Organogram BPH
-  { image: "https://gdurl.com/WzNQ",  }    //Organogram Geopoint
-]
+  { image: "https://gdurl.com/1G1y" }, //Organogram BPH
+  { image: "https://gdurl.com/WzNQ" }, //Organogram Geopoint
+];
 
-export default SliderData
+export default SliderData;
