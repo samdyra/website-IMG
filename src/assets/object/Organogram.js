da7ceaeaacccd8acdb0e4357e033a7d7b9f3170e
alshaadiq <alshaaadiq@gmail.com>
2024-01-05 20:40:06 +0700

    Add Organogram Timsen

--- src/assets/object/Organogram.js
+++ src/assets/object/Organogram.js
@@ -1,6 +1,7 @@
 const SliderData = [
   { image: "https://gdurl.com/1G1y" }, //Organogram BPH
   { image: "https://gdurl.com/WzNQ" }, //Organogram Geopoint
+  { image: "https://gdurl.com/25IG"}, //Organogram Tim Senator
 ];
 
 export default SliderData;
